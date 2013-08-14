ef70bce30d3b885b2216e18f7da95ab77ec36f6b
Abhijeet Pandit <abhijeetp@apconic.com>
2013-08-14 13:32:43 +0530

    Added new project

---
+++ springdemo/src/main/java/org/abhijeet/springdemo/App.java
@@ -0,0 +1,13 @@
+package org.abhijeet.springdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
